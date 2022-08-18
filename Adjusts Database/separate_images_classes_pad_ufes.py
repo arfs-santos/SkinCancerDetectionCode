15fcda2ef26e6172385918a659db69442633c606
Alan Santos <91705458+arfs-santos@users.noreply.github.com>
2022-08-18 20:41:41 -0300

    terceiro commit

---
+++ Adjusts Database/separate_images_classes_pad_ufes.py
@@ -0,0 +1,51 @@
+import numpy as np
+import shutil
+import os 
+import pandas as pd 
+from pathlib import PurePath
+import cv2
+import csv
+
+path_class = 'D:/Bases de Imagens/PAD-UFES/images_class.csv'
+
+path_names = 'D:/Bases de Imagens/PAD-UFES/images_names.csv'
+
+images_class = pd.read_csv(path_class)
+
+images_class = images_class.diagnostic
+
+images_names = pd.read_csv(path_names)
+
+images_names = images_names.img_id
+
+data = []
+
+for name in images_names:
+    d = os.path.join('D:/Bases de Imagens/PAD-UFES/images/', 
+                    name)
+    data.append(d)
+
+path_malign = 'D:/Bases de Imagens/PAD-UFES/malign'
+
+path_benign = 'D:/Bases de Imagens/PAD-UFES/benign'     
+
+it = 0
+
+for cl in images_class:
+    if cl == 'MEL' or cl == 'BCC' or cl =='SCC':
+        nome = PurePath(data[it]).name
+        dest = os.path.join(path_malign, nome)
+        shutil.copy(data[it], dest)
+        it= it+1
+        continue   
+    
+    elif cl =='ACK' or cl == 'NEV' or cl == 'SEK':
+       nome = PurePath(data[it]).name
+       dest = os.path.join(path_benign, nome)
+       shutil.copy(data[it], dest)
+       it= it+1
+       continue
+   
+    else:
+       break
+   
