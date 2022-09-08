1a17df02b15531729033b1ecf181e86f6ae0492b
Alan Santos <91705458+arfs-santos@users.noreply.github.com>
2022-09-08 09:44:55 -0300

    Update hair_removal.py

--- SkinCancerDetectionCode/Ajuste das Bases/Remoção de Pelos/hair_removal.py
+++ SkinCancerDetectionCode/Ajuste das Bases/Remoção de Pelos/hair_removal.py
@@ -1,17 +1,25 @@
 
 import cv2
 import os 
+from pathlib import PurePath
+import shutil
 import numpy as np
-from skimage.restoration import inpaint
 
+
+# Path dataset ISIC 2020 and PAD-UFES
 
 path_isic = os.listdir('D:/Bases de Imagens/ISIC2020/ISIC_2020_Training_JPEG')
 
 path_pad_ufes = os.listdir('D:/Bases de Imagens/PAD-UFES/images')
 
+
+# Path out hair removal images ISIC2020 and PAD-UFES
+
 out_dir_isic = 'D:/Bases de Imagens/ISIC2020/ISIC_2020_hair_removal/'
 
 out_dir_pad_ufes = 'D:/Bases de Imagens/PAD-UFES/images_hair_removal/'
+
+# Aux variables
 
 data_isic = []
 
@@ -27,36 +35,42 @@ for name in path_pad_ufes:
                     name)
     data_pad_ufes.append(d)
 
+# Kernel type and size 
 
 kernel_fill = cv2.getStructuringElement(1,(23,23))
 kernel_dilatate = cv2.getStructuringElement(2,(3,3))
 kernel = cv2.getStructuringElement(2,(21,21))
 
+# Percent images resize
+
 scala = 50 
 
-
+# aux count
   
 i = 0
-    
+
+# Loop remove hair
+ 
 for im in data_isic:
-    
     img = cv2.imread(im, cv2.IMREAD_COLOR)
-    largura = int(im.shape[1] * scala / 100)
-    altura = int(im.shape[0] * scala / 100)
-    dim = (largura, altura)
     grayScale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     blackhat = cv2.morphologyEx(grayScale, cv2.MORPH_BLACKHAT, kernel_fill)
-    bhg= cv2.GaussianBlur(blackhat,(17,17),cv2.BORDER_DEFAULT)
+    bhg= cv2.GaussianBlur(blackhat,(19,19),cv2.BORDER_DEFAULT)
     ret,mask = cv2.threshold(bhg,10,255,cv2.THRESH_BINARY)
-    mask = cv2.dilate(mask, kernel_dilatate, iterations = 4)
+    mask = cv2.dilate(mask, kernel_dilatate, iterations = 3)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     num_non_zero = cv2.countNonZero(mask)
-    if num_non_zero > 50:
-        dst = cv2.inpaint(img,mask,1,cv2.INPAINT_TELEA)
+    if num_non_zero > 20000:
+        dst = cv2.inpaint(img,mask,3,cv2.INPAINT_TELEA)
         aux = os.path.split(data_isic[i])
         path = (out_dir_isic + str(aux[1]))
         cv2.imwrite(path, dst)
-    i = i+1
+        i = i+1
+    else:
+        aux = os.path.split(data_isic[i])
+        path = (out_dir_isic + str(aux[1]))
+        cv2.imwrite(path, img)
+        i=i+1
     
 
 # i =0
